99a4fdce11f49b44f40c1a1a99d69aef3305e470
poswalabhishek <poswalabhishek1234@icloud.com>
2021-01-06 21:44:31 +0800

    resumePDF

--- back.js
+++ back.js
@@ -3,18 +3,22 @@ var interval;
 
 
 for (var i = 0; i < navMenuAnchorTags.length; i++) {
-    navMenuAnchorTags[i].addEventListener('click', function (event) {
-        event.preventDefault();
-        var targetSectionID = this.textContent.trim().toLowerCase();
-        console.log(this.textContent);
-        var targetSection = document.getElementById(targetSectionID);
-        console.log(targetSection);
-        //    interval = setInterval(scrollVertically, 20, targetSection);
+    if (i == 0 || i == 8) {
+        continue;
+    } else { 
+        navMenuAnchorTags[i].addEventListener('click', function (event) {
+            event.preventDefault();
+            var targetSectionID = this.textContent.trim().toLowerCase();
+            console.log(this.textContent);
+            var targetSection = document.getElementById(targetSectionID);
+            console.log(targetSection);
+            //    interval = setInterval(scrollVertically, 20, targetSection);
 
-        interval = setInterval(function () {
-            scrollVertically(targetSection);
-        }, 20);
-    });
+            interval = setInterval(function () {
+                scrollVertically(targetSection);
+            }, 20);
+        });
+    }
 }
 
 
